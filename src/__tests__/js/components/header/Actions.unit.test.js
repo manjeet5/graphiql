7d47206c943708d3910fa8b071fbb5228639a1bb
manjeet5 <manjeet175@gmail.com>
2020-06-18 11:30:56 -0400

    added test cases for actions component

---
+++ src/__tests__/js/components/header/Actions.unit.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Actions, {
+  createButton,
+} from "../../../../js/components/header/Actions";
+import {
+  CREATE_QUERY_REQUEST_BODY,
+  SAVE_QUERY_TO_LOCAL_STORAGE,
+  TOGGLE_SHOW_QUERY_LIST_HISTORY,
+} from "../../../../js/components/store/reducer";
+import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+
+describe("Actions", () => {
+  let dispatch;
+  beforeEach(() => {
+    dispatch = jest.fn();
+  });
+  it("render", () => {
+    render(<Actions dispatch={dispatch} />);
+    const [runButton, saveButton, historyButton] = screen.getAllByRole(
+      "button"
+    );
+    expect(runButton).toHaveAttribute("id", "run");
+    expect(saveButton).toHaveAttribute("id", "save");
+    expect(historyButton).toHaveAttribute("id", "history");
+
+    //invoke click on runButton
+    act(() => {
+      runButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(dispatch.mock.calls[0][0].type).toEqual(CREATE_QUERY_REQUEST_BODY);
+
+    //invoke click on saveButton
+    act(() => {
+      saveButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(dispatch.mock.calls[1][0].type).toEqual(SAVE_QUERY_TO_LOCAL_STORAGE);
+
+    //invoke click on saveButton
+    act(() => {
+      historyButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(dispatch.mock.calls[2][0].type).toEqual(
+      TOGGLE_SHOW_QUERY_LIST_HISTORY
+    );
+  });
+});
