104126b8ec4755bfc4d3592e38238e89d8b58190
manjeet5 <manjeet175@gmail.com>
2020-06-18 08:56:55 -0400

    added resizing capability

--- src/js/components/editor/Editor.js
+++ src/js/components/editor/Editor.js
@@ -1,13 +1,46 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import QueryEditor from "./QueryEditor";
 import QueryResult from "./QueryResult";
-const Editor = ({ store, dispatch }) => {
-  const { activeQuery, requestBody } = store;
-  const editor = useRef(null);
 
+const Editor = ({ store, dispatch }) => {
+  const { activeQuery, requestBody, queryEditorRef, queryResultRef } = store;
+  const editor = useRef(null);
+  const initResize = (event) => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResize);
+  };
+
+  const widthInPercentage = (width, totalWidth) => {
+    return (width / totalWidth) * 100 + "%";
+  };
+  const resize = (event) => {
+    const totalWidth = editor.current.getBoundingClientRect().width;
+    const difference =
+      event.clientX - queryEditorRef.current.getBoundingClientRect().width;
+    const queryEditorWidth =
+      queryEditorRef.current.getBoundingClientRect().width + difference;
+    const queryResultWidth = totalWidth - queryEditorWidth - 10; // 10 refers to the width of resize-editor;
+    queryEditorRef.current.style.width = widthInPercentage(
+      queryEditorWidth,
+      totalWidth
+    );
+    queryResultRef.current.style.width = widthInPercentage(
+      queryResultWidth,
+      totalWidth
+    );
+  };
+  const stopResize = () => {
+    window.removeEventListener("mousemove", resize);
+    window.removeEventListener("mouseup", stopResize);
+  };
+
+  useEffect(() => {
+    return stopResize;
+  });
   return (
     <div className="editor-container" ref={editor}>
       <QueryEditor query={activeQuery} dispatch={dispatch} />
+      <div className="resize-editor" onMouseDown={initResize}></div>
       <QueryResult requestBody={requestBody} dispatch={dispatch} />
     </div>
   );
