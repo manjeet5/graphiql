fb6bce08207742e2620c3e7250645a9e59e29f6f
manjeet5 <manjeet175@gmail.com>
2020-06-18 21:16:58 -0400

    added placeholder text for queryeditor

--- src/js/components/editor/QueryEditor.js
+++ src/js/components/editor/QueryEditor.js
@@ -17,6 +17,28 @@ const QueryEditor = ({ query, dispatch }) => {
     dispatch({ type: ADD_QUERY_EDITOR_REF, payload: queryEditorRef });
   }, [dispatch]);
 
+  const placeholder = `
+  # Welcome to GraphiQL
+  #
+  # GraphiQL is an in-browser tool for writing, validating, and
+  # testing GraphQL queries.
+  #
+  # Type queries into this side of the screen
+  #
+  # GraphQL queries typically start with a "{" character. Lines that start
+  # with a # are ignored.
+  #
+  # Buttons will become active as you provide baseurl and query information
+  #
+  # An example GraphQL query might look like:
+  #
+  #     {
+  #       field(arg: "value") {
+  #         subField
+  #       }
+  #     }
+  #
+  `;
   return (
     <div className="query-editor" ref={queryEditorRef}>
       <textarea
@@ -24,6 +46,7 @@ const QueryEditor = ({ query, dispatch }) => {
         className="query-editor-text-area"
         value={query}
         ref={textAreaRef}
+        placeholder={placeholder}
         onChange={handleChange}
       />
     </div>
