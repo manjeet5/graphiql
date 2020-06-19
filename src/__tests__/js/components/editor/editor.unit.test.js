0ab684de4d2fd2a3e1d21b90670b054d3b7968a3
manjeet5 <manjeet175@gmail.com>
2020-06-19 01:42:07 -0400

    fixed test cases

--- src/__tests__/js/components/editor/editor.unit.test.js
+++ src/__tests__/js/components/editor/editor.unit.test.js
@@ -3,6 +3,12 @@ import Editor from "../../../../js/components/editor/Editor";
 import { shallow } from "../../../../enzyme";
 
 test("Editor should render QueryEditor", () => {
-  const wrapper = shallow(<Editor store={{}} />);
+  const store = {
+    activeQuery: "",
+    baseUrl: "",
+    requestBody: "",
+    showQueryListHistory: false,
+  };
+  const wrapper = shallow(<Editor store={store} dispatch={jest.fn()} />);
   expect(wrapper.find("QueryEditor").exists()).toEqual(true);
 });
